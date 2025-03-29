7e2042a8b18c135adcf445e328c7a3d1c48332a0
1024小神 <15670339118@163.com>
2025-03-29 18:53:38 +0800

    first commit

---
+++ PakePlus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  PakePlus
+//
+//  Created by Song on 2025/3/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, PakePLus!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ PakePlus/PakePlusApp.swift
@@ -0,0 +1,17 @@
+//
+//  PakePlusApp.swift
+//  PakePlus
+//
+//  Created by Song on 2025/3/29.
+//
+
+import SwiftUI
+
+@main
+struct PakePlusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
