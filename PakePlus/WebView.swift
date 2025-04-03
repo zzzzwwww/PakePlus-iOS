ba4f5d14a32923da615ff5e4ba3f03099d60ff8c
1024小神 <15670339118@163.com>
2025-04-03 16:58:06 +0800

    Add swipe gesture recognizers to WebView for navigation functionality

--- PakePlus/WebView.swift
+++ PakePlus/WebView.swift
@@ -22,6 +22,16 @@ struct WebView: UIViewRepresentable {
         let scriptInjection = WKUserScript(source: script, injectionTime: .atDocumentEnd, forMainFrameOnly: true)
         webView.configuration.userContentController.addUserScript(scriptInjection)
         webView.load(URLRequest(url: url))
+        
+        // Add gesture recognizers
+        let rightSwipeGesture = UISwipeGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleRightSwipe(_:)))
+        rightSwipeGesture.direction = .right
+        webView.addGestureRecognizer(rightSwipeGesture)
+        
+        let leftSwipeGesture = UISwipeGestureRecognizer(target: context.coordinator, action: #selector(Coordinator.handleLeftSwipe(_:)))
+        leftSwipeGesture.direction = .left
+        webView.addGestureRecognizer(leftSwipeGesture)
+        
         return webView
     }
 
@@ -38,6 +48,18 @@ struct WebView: UIViewRepresentable {
     class Coordinator: NSObject, UIScrollViewDelegate {
         func viewForZooming(in scrollView: UIScrollView) -> UIView? {
             return nil // 返回 nil 禁止缩放
+        }
+        
+        @objc func handleRightSwipe(_ gesture: UISwipeGestureRecognizer) {
+            if let webView = gesture.view as? WKWebView, webView.canGoBack {
+                webView.goBack()
+            }
+        }
+        
+        @objc func handleLeftSwipe(_ gesture: UISwipeGestureRecognizer) {
+            if let webView = gesture.view as? WKWebView, webView.canGoForward {
+                webView.goForward()
+            }
         }
     }
 }
